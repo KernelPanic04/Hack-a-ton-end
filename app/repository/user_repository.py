2bc1b0b9cf30af33aecd1a71442591c1250f080a
MinusTatan234 <ikerbaruch@hotmail.com>
2026-08-22 15:23:13 -0600

    feat: repository base added

---
+++ app/repository/user_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.models.user import User
+from app.repository.base import BaseRepository
+
+class UserRepository(BaseRepository[User]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(User, session)
+
+    # Here we add personalized queries specific to this entity
+    # Aquí agregas consultas personalizadas específicas de esta entidad
+    async def get_by_email(self, email: str) -> User | None:
+        query = select(User).where(User.email == email)
+        result = await self.session.execute(query)
+        return result.scalars().first()
